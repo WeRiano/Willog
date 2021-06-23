f184f0241a0cee005ae6f6331f3ae374a444341c
Weriano <williamfors13@gmail.com>
2021-06-23 02:47:41 +0200

    first commit

---
+++ test/Test.cpp
@@ -0,0 +1,36 @@
+﻿#include "Willog.hpp"
+#include "ProgBar.hpp"
+
+#include <chrono>
+#include <thread>
+
+void DoWork()
+{
+    volatile int k = 0;
+    for(int i = 0; i < 10000; i++)
+    {
+        for(int j = 0; j < 100000; j++)
+        {
+            k = i*j;
+        }
+    }
+}
+
+void TestProgBar()
+{
+    unsigned int l = 19;
+    Willog::ProgBar pb = Willog::ProgBar(l, Willog::ProgBarType::squares);
+    for(unsigned int i = 1; i <= 27 + 1; i++)
+    {
+        pb.Display();
+        Sleep(500);
+        pb.SetProg((double) (i) / 27 );
+    }
+}
+
+int main()
+{
+    Willog::Init();
+
+    TestProgBar();
+}
