3c2b0445d2c8b5a3891b79daa071b99e26fb7493
William Fors <williamfors13@gmail.com>
2021-06-23 03:14:56 +0200

    Delete bin directory

--- bin/debug/src/willog/CMakeFiles/core.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "Core.lib"
-  "Core.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Core.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- bin/release/src/willog/CMakeFiles/willog.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "Willog.lib"
-  "Willog.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Willog.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
