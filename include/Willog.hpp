493be6522cda04d5459bd9f43299cb8670eb08f4
Weriano <williamfors13@gmail.com>
2021-06-26 18:01:25 +0200

    fix file loc and line nr + added func name

--- include/Willog.hpp
+++ include/Willog.hpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <string>
 
+#define __cpp_consteval
+#include <source_location>
+
 #if defined(_WIN32) || defined(_WIN64)
     #include <Windows.h>
 #else
@@ -10,6 +13,15 @@
 #endif
 
 namespace Willog {
+
+    struct LogInfo
+    {
+        std::string fileName, functionName;
+        unsigned int line;
+        LogInfo(std::string fen, std::string fnn, unsigned int l)
+            : fileName(fen), functionName(fnn), line(l)
+        {}
+    };
 
     enum LogColor
     {
@@ -26,12 +38,13 @@ namespace Willog {
     class State
     {
     private:
-        static bool s_ShowFP;
-        static bool s_ShowLine;
-        static LogLevel s_Level;
+        inline static bool s_ShowFP = false;
+        inline static bool s_ShowLine = false;
+        inline static bool s_ShowFunc = false;
+        inline static LogLevel s_Level = LogLevel::ALL;
 
     public:
-        static HANDLE s_Console;
+        inline static HANDLE s_Console;
 
     public:
         inline static bool CheckLevel(LogLevel l)
@@ -49,12 +62,11 @@ namespace Willog {
         inline static void ShowLine() { s_ShowLine = true; }
         inline static void HideLine() { s_ShowLine = false; }
         inline static bool IsLineShowing() { return s_ShowLine; }
-    };
 
-    inline LogLevel State::s_Level = LogLevel::ALL;
-    inline HANDLE State::s_Console;
-    inline bool State::s_ShowFP = true;
-    inline bool State::s_ShowLine = true;
+        inline static void ShowFunc() { s_ShowFunc = true; }
+        inline static void HideFunc() { s_ShowFunc = false; }
+        inline static bool IsFuncShowing() { return s_ShowFunc; }
+    };
 
     inline State s_LOG_INST;
 
@@ -63,9 +75,7 @@ namespace Willog {
 #if defined(_WIN32) || defined(_WIN64)
         SetConsoleOutputCP(CP_UTF8);
 #endif
-
         // Make output faster by ...
-
         std::ios::sync_with_stdio(false); // toggle on or off the synchronization of all the C++ standard streams with their corresponding standard C streams
         std::cin.tie(NULL); // guarantee the flushing of std::cout before std::cin accepts an input
         std::cout.tie(NULL); // ... and vise-versa
@@ -74,28 +84,35 @@ namespace Willog {
         State::s_Console = GetStdHandle(STD_OUTPUT_HANDLE);
     }
 
-    static inline void LogMsg(LogLevel level, std::string s)
+    static inline void LogMsg(LogLevel level, std::string s, LogInfo info)
     {
         if (State::CheckLevel(level))
         {
             if(State::IsFPShowing()) {
-                std::cout << "[" << __FILE__ << "] ";
+                std::cout << "[" << info.fileName << "] ";
+            }
+            if(State::IsFuncShowing())
+            {
+                std::cout << "[" << info.functionName << "] ";
             }
             if(State::IsLineShowing()) {
-                std::cout << "(Line: " << __LINE__ << ") ";
+                std::cout << "(Line: " << info.line << ") ";
             }
             std::cout << s << '\n';
         }
     }
 
+    // These exist because I dont want the user to have to use the State class. TODO: Maybe rethink?
     inline void SetLogLevel(LogLevel level) { State::SetLevel(level); }
     inline LogLevel GetLogLevel() { return State::GetLevel(); }
     inline void HideFP() { State::HideFP(); }
     inline void ShowFP() { State::ShowFP(); }
     inline void HideLine() { State::HideLine(); }
     inline void ShowLine() { State::ShowLine(); }
+    inline void HideFunc() { State::HideFunc(); }
+    inline void ShowFunc() { State::ShowFunc(); }
 
-    static inline int GetColorAttrib(LogColor color) // Get Color code?
+    static inline int GetColorAttrib(LogColor color) // "GetColorCode"
     {
         switch (color) {
             case BLACK:
@@ -119,42 +136,42 @@ namespace Willog {
         }
     }
 
-    inline void SetColor(LogColor color)
+    static inline void SetColor(LogColor color)
     {
         SetConsoleTextAttribute(State::s_Console, GetColorAttrib(color));
     }
 
-    inline void Fatal(std::string s)
+    inline void Fatal(std::string s, std::source_location sl = std::source_location::current())
     {
         SetColor(RED);
         s = "FATAL: " + s;
-        LogMsg(LogLevel::FATAL, s);
+        LogMsg(LogLevel::FATAL, s, LogInfo(sl.file_name(), sl.function_name(), sl.line()));
     }
-    inline void Error(std::string s) {
+    inline void Error(std::string s, std::source_location sl = std::source_location::current()) {
         SetColor(RED);
         s = "ERROR: " + s;
-        LogMsg(LogLevel::ERR, s);
+        LogMsg(LogLevel::ERR, s, LogInfo(sl.file_name(), sl.function_name(), sl.line()));
     }
-    inline void Warning(std::string s)
+    inline void Warning(std::string s, std::source_location sl = std::source_location::current())
     {
         SetColor(YELLOW);
         s = "WARN: " + s;
-        LogMsg(LogLevel::WARN, s);
+        LogMsg(LogLevel::WARN, s, LogInfo(sl.file_name(), sl.function_name(), sl.line()));
     }
-    inline void Info(std::string s) {
+    inline void Info(std::string s, std::source_location sl = std::source_location::current()) {
         SetColor(WHITE);
         s = "INFO: " + s;
-        LogMsg(LogLevel::INFO, s);
+        LogMsg(LogLevel::INFO, s, LogInfo(sl.file_name(), sl.function_name(), sl.line()));
     }
-    inline void Debug(std::string s) {
+    inline void Debug(std::string s, std::source_location sl = std::source_location::current()) {
         SetColor(GREEN);
         s = "DEBUG: " + s;
-        LogMsg(LogLevel::DEBUG, s);
+        LogMsg(LogLevel::DEBUG, s, LogInfo(sl.file_name(), sl.function_name(), sl.line()));
     }
-    inline void Trace(std::string s) {
+    inline void Trace(std::string s, std::source_location sl = std::source_location::current()) {
         SetColor(WHITE);
         s = "TRACE: " + s;
-        LogMsg(LogLevel::TRACE, s);
+        LogMsg(LogLevel::TRACE, s, LogInfo(sl.file_name(), sl.function_name(), sl.line()));
     }
 
     inline double Clamp(double v, double max, double min)
